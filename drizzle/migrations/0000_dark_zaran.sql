d9b040096bd97f145c10782a2e6cbba2a04c7bbc
Phuc Lam <hplam130597@gmail.com>
2026-01-21 17:25:58 +0700

    Add initial database schema and migration files for event management

---
+++ drizzle/migrations/0000_dark_zaran.sql
@@ -0,0 +1,46 @@
+CREATE TABLE `event_signups` (
+	`event_id` integer NOT NULL,
+	`user_id` integer NOT NULL,
+	`signed_up_at` integer,
+	PRIMARY KEY(`event_id`, `user_id`),
+	FOREIGN KEY (`event_id`) REFERENCES `events`(`id`) ON UPDATE no action ON DELETE cascade,
+	FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE cascade
+);
+--> statement-breakpoint
+CREATE TABLE `events` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`region` text NOT NULL,
+	`week_start_date` integer NOT NULL,
+	`created_at` integer
+);
+--> statement-breakpoint
+CREATE TABLE `team_members` (
+	`team_id` integer NOT NULL,
+	`user_id` integer NOT NULL,
+	`assigned_at` integer,
+	PRIMARY KEY(`team_id`, `user_id`),
+	FOREIGN KEY (`team_id`) REFERENCES `teams`(`id`) ON UPDATE no action ON DELETE cascade,
+	FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE cascade
+);
+--> statement-breakpoint
+CREATE TABLE `teams` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`event_id` integer NOT NULL,
+	`name` text NOT NULL,
+	`description` text,
+	`created_at` integer,
+	FOREIGN KEY (`event_id`) REFERENCES `events`(`id`) ON UPDATE no action ON DELETE cascade
+);
+--> statement-breakpoint
+CREATE TABLE `users` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`username` text NOT NULL,
+	`password` text,
+	`is_admin` integer DEFAULT false,
+	`classes` text,
+	`role` text,
+	`region` text NOT NULL,
+	`created_at` integer
+);
+--> statement-breakpoint
+CREATE UNIQUE INDEX `users_username_unique` ON `users` (`username`);
